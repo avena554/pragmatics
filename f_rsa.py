76caeb0cb78fc3dd939683eacfe73d10f28b2d11
antoine <imac48@dhcp104-196.coli.uni-saarland.de>
2017-11-30 19:41:22 +0100

    not an empty message

---
+++ f_rsa.py
@@ -0,0 +1,13 @@
+import math
+from math import exp
+
+
+def ls0(types, messages, lexicon, alpha):
+    for(m in messages):
+        j = 0
+        if(t in lexicon[m]):
+            j = exp(alpha*)
+    
+
+def nextL(types, lexicon, seq):
+    
